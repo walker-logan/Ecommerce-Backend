6d235322879dc14cdeed4a7f07fd434c783d2fea
Walker Logan <walker.logan44@gmail.com>
2023-05-25 23:02:43 -0500

    added category-seeds.js

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,1 +1,23 @@
-// category-seeds
+const { Category } = require('../models');
+
+const categoryData = [
+  {
+    category_name: 'Electronics',
+  },
+  {
+    category_name: 'Books',
+  },
+  {
+    category_name: 'Clothing',
+  },
+  {
+    category_name: 'Kitchen',
+  },
+  {
+    category_name: 'Office-Supplies',
+  },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
